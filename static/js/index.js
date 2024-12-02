5d3bc3e7bb6e6e1ee2ea47948fb8e7a9a1ef2c7b
zaozao <709800389@qq.com>
2024-12-02 11:33:40 +0800

    first commit

--- static/js/index.js
+++ static/js/index.js
@@ -38,7 +38,7 @@ function setInterpolationImage2(i) {
 
 
 $(document).ready(function() {
-    console.log('$(document).ready')
+    // console.log('$(document).ready')
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
